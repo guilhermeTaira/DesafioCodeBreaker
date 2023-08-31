8a51e0890db07c666fc31a2b9261dc68f83f63af
Guilherme.Taira <gui.s.taira@live.com>
2023-08-30 22:43:19 -0300

    melhoria no código e adequação ao novo desafio

--- DesafioCodeBreaker/Program.cs
+++ DesafioCodeBreaker/Program.cs
@@ -8,73 +8,84 @@ namespace DesafioCodeBreaker
     {
         private static readonly int _minKeyNumber = 1;
         private static readonly int _maxKeyNumber = 10000;
-        private static readonly string _apiUrl = "https://localhost:7038/fiap";
+        private static readonly string _apiUrl = "https://fiap-inaugural.azurewebsites.net/fiap";
 
-        private static int _counter = 0;
+        static Random _random = new Random();
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var keys = GenerateShuffledKeys();
+            var intList = GenerateShuffledIntList();
 
-            CancellationTokenSource cts = new();
-
-            var options = new ParallelOptions()
+            foreach (int i in intList)
             {
-                CancellationToken = cts.Token,
-                MaxDegreeOfParallelism = Environment.ProcessorCount
-            };
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Numero testado: {i}");
 
-            Parallel.ForEach(keys, options, async key =>
-            {
-                var validKey = await ValidateKey(key);
+                var keys = GenerateShuffledPossibleKeys(i);
 
-                if (validKey)
+                var httpClient = new HttpClient();
+
+                var tasks = new List<Task<(string, bool)>>();
+                foreach (var key in keys)
+                    tasks.Add(ValidateKey(httpClient, key));
+
+                (string, bool)[] responses = await Task.WhenAll(tasks);
+
+                var successKey = responses.FirstOrDefault(r => r.Item2);
+
+                if (successKey.Item1 != null)
                 {
-                    Console.WriteLine($"////////////////////////CHAVE ENCONTRADA: {key}");
-                    cts.Cancel();
+                    Console.WriteLine($"////////////////////////CHAVE ENCONTRADA: {successKey}");
+                    break;
                 }
-                else
-                {
-                    Console.WriteLine($"[{_counter}/{keys.Count}]: {key}");
-                }
-            });
+            }
 
             Console.ReadKey();
         }
 
-        static List<string> GenerateShuffledKeys()
+        static List<string> GenerateShuffledPossibleKeys(int number)
         {
-            char[] chars = { 'a', 'á', 'à', 'â', 'ã', 'b', 'c', 'ç', 'd', 
-                             'e', 'é', 'è', 'ê', 'f', 'g', 'h', 'i', 'í', 
-                             'ì', 'î', 'j', 'k', 'l', 'm', 'n', 'o', 'ó', 
-                             'ò', 'ô', 'õ', 'p', 'q', 'r', 's', 't', 'u', 
-                             'ú', 'ù', 'û', 'v', 'w', 'x', 'y', 'z' };
+            var chars = new List<char>
+                { 'a', 'á', 'à', 'â', 'ã', 'b', 'c', 'ç', 'd',
+                  'e', 'é', 'è', 'ê', 'f', 'g', 'h', 'i', 'í',
+                  'ì', 'î', 'j', 'k', 'l', 'm', 'n', 'o', 'ó',
+                  'ò', 'ô', 'õ', 'p', 'q', 'r', 's', 't', 'u',
+                  'ú', 'ù', 'û', 'v', 'w', 'x', 'y', 'z' };
+
+            //capital letters
+            chars.AddRange(
+                chars.Select(c => char.ToUpper(c)).ToList()
+            );
 
             List<string> keys = new List<string>();
 
-            for(int number = _minKeyNumber; number <= _maxKeyNumber; number++)
+
+            foreach (var chrLeft in chars)
             {
-                foreach(var chr in chars)
+                foreach (var chrRight in chars)
                 {
-                    keys.Add($"{number}{chr}");
-                    keys.Add($"{chr}{number}");
-                    keys.Add($"{number}{chr.ToString().ToUpper()}");
-                    keys.Add($"{chr.ToString().ToUpper()}{number}");
+                    keys.Add($"{chrLeft}{number}{chrRight}");
                 }
+
+                keys.Add($"{chrLeft}{number}");
+                keys.Add($"{number}{chrLeft}");
             }
 
-            keys = keys.OrderBy(a => Guid.NewGuid()).ToList();
-
-            return keys;
+            return Shuffle(keys);
         }
 
-        static async Task<bool> ValidateKey(string key)
+        static List<int> GenerateShuffledIntList()
         {
-            var httpClient = new HttpClient();
+            var list = Enumerable.Range(_minKeyNumber,
+                                        _maxKeyNumber - _minKeyNumber + 1).ToList();
 
+            return Shuffle(list);
+        }
+
+        static async Task<(string, bool)> ValidateKey(HttpClient httpClient, string key)
+        {
             var httpContent = new StringContent(
                                     $"{{\"key\": \"{key}\"}}",
-                                    Encoding.UTF8, 
+                                    Encoding.UTF8,
                                     "application/json");
 
             var response = await Policy
@@ -82,17 +93,30 @@ namespace DesafioCodeBreaker
                                     .OrResult<HttpResponseMessage>(message => message.StatusCode != HttpStatusCode.OK && message.StatusCode != HttpStatusCode.NoContent)
                                     .WaitAndRetryAsync(new[]
                                     {
-                                        TimeSpan.FromSeconds(1),
                                         TimeSpan.FromSeconds(3),
                                         TimeSpan.FromSeconds(9),
                                         TimeSpan.FromSeconds(15),
-                                    }, (result, timeSpan, retryCount, context) => {
+                                    }, (result, timeSpan, retryCount, context) =>
+                                    {
                                         Console.WriteLine($"key {key} failed, retrycount = {retryCount}. waiting {timeSpan} before next retry.");
                                     })
                                     .ExecuteAsync(() => httpClient.PostAsync(_apiUrl, httpContent));
 
-            _counter++;
-            return response.StatusCode == HttpStatusCode.OK;
+            return (key, response.StatusCode == HttpStatusCode.OK);
+        }
+
+        static List<T> Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            for (int i = n - 1; i > 0; i--)
+            {
+                int j = _random.Next(i + 1);
+                T temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+            return list;
         }
     }
 }
